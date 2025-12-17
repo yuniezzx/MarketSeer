b6bb61dcccf39591a13bbe947d7701c491e7fcf4
yuniezzx <maxzzx224@gmail.com>
2025-12-18 00:04:22 +0800

    add shadcn ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-16'>
           {/* Logo/Brand */}
-          <div className='flex-shrink-0'>
+          <div className='shrink-0'>
             <h1 className='text-xl font-bold text-gray-900 dark:text-white'>MarketSeer</h1>
           </div>
 
